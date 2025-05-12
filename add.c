6a611a1918962ec1cda6ef91c4d71d87410ccf28
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-12 14:27:26 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h> 
+int main(){
+ int a,b,sum=0;
+ printf("enter the value of a");
+ scanf("%d",&a);
+ printf("enter the value of b");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("the sum of is%d",sum);
+}
